47e8e76ee401f1714e10e66207c52df4749d8d4a
TeamBocean <112326837+TeamBocean@users.noreply.github.com>
2023-09-27 20:46:11 +0000

    menu functionality

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,27 +1,159 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import kanafeh from "../assets/images/classic.png";
+import baklawa from "../assets/images/baklawa.png";
+import "./Reviews.css"
 
-export default function Menu() {
-  const [activeSection, setActiveSection] = useState("Pistachio Baklawa");
+const Menu = () => {
+  const [selectedOption, setSelectedOption] = useState("Pistachio Baklawa");
 
-  const handleButtonClick = (sectionName) => {
-    setActiveSection(sectionName);
+  const [before, setBefore] = useState(4);
+  const [after, setAfter] = useState(1);
+
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+    arr.indexOf(selectedOption);
   };
 
+  const arr = [{name:"Pistachio Baklawa"},{name:"Classic Kanafeh"}, {name: "Pistachio Kanafeh"}, {name:"Nabulsi Kanafeh"},{name: "Basbusa"}];
+
+  const position = (arr, pos) => {
+    let before;
+    let after;
+    if (pos === 0) {
+      before = arr.length - 1;
+    } else {
+      before = pos - 1;
+    }
+
+    after = pos + 1;
+
+    if (after === arr.length || after === arr.length + 1) {
+      after = 0;
+    }
+
+    setAfter(after);
+    setBefore(before);
+  };
+
+  useEffect(() => {
+    let pos;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].name === selectedOption) {
+        pos = i; 
+      }
+    }
+    position(arr,pos);
+  }, [selectedOption]);
+
   return (
-    <section className="bg-black text-white">
-      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 text-black font-semibold">
-        <div className="text-center">
-          <h1 className="text-3xl font-bold pb-2 border-b-2 border-orange inline-block text-center text-white capitalize dark:text-white mb-4">
-            Our Menu
-          </h1>
+    <section className="mb-20 bg-black flex flex-col mt-32">
+      <div className="flex my-10 mx-auto">
+        <button
+          type="button"
+          class={`${
+            selectedOption === "Pistachio Baklawa"
+              ? "bg-[#FFC300] text-black"
+              : "text-black bg-white"
+          } hover:bg-[#FFC300]focus:outline-none   font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 `}
+          onClick={() => {
+            handleOptionChange("Pistachio Baklawa");
+          }}
+        >
+          Pistachio Baklawa
+        </button>
+        <button
+          type="button"
+          class={`${
+            selectedOption === "Classic Kanafeh"
+              ? "bg-[#FFC300] text-black"
+              : "text-black bg-white"
+          } hover:bg-[#FFC300] focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 `}
+          onClick={() => {
+            handleOptionChange("Classic Kanafeh");
+          }}
+        >
+          Classic Kanafeh
+        </button>
+        <button
+          type="button"
+          class={`${
+            selectedOption === "Pistachio Kanafeh"
+              ? "bg-[#FFC300] text-black"
+              : "text-black bg-white"
+          } hover:bg-[#FFC300] focus:outline-none   font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 `}
+          onClick={() => {
+            handleOptionChange("Pistachio Kanafeh");
+          }}
+        >
+          Pistachio Kanafeh
+        </button>
+        <button
+          type="button"
+          class={`${
+            selectedOption === "Nabulsi Kanafeh"
+              ? "bg-[#FFC300] text-black"
+              : "text-black bg-white"
+          } hover:bg-[#FFC300] focus:outline-none   font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 `}
+          onClick={() => {
+            handleOptionChange("Nabulsi Kanafeh");
+          }}
+        >
+          Nabulsi Kanafeh
+        </button>
+        <button
+          type="button"
+          class={`${
+            selectedOption === "Basbusa"
+              ? "bg-[#FFC300] text-black"
+              : "text-black bg-white"
+          } hover:bg-[#FFC300] focus:outline-none   font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 `}
+          onClick={() => {
+            handleOptionChange("Basbusa");
+          }}
+        >
+          Basbusa
+        </button>
+      </div>
+      <div className="flex flex-col justify-center content-center sm:flex-row gap-4 sm:mx-10">
+        <div class="w-full self-center max-w-sm shadow container-one">
+          <div class="flex flex-col items-center pt-4 pb-10">
+            <img
+              class="w-36 h-36 mb-3 rounded-full shadow-lg"
+              src={baklawa}
+              alt="Bonnie image"
+            />
+            <h5 class="mb-1 text-xl font-medium text-white">
+              {arr[before].name}
+            </h5>
+          </div>
         </div>
-
-        
-
-        <div className="text-center mt-8">
-          <button className="mx-auto px-8 py-2 bg-orange text-black">View Menu</button>
+        <div class="self-center w-full max-w-sm container-two">
+          <div class="flex flex-col items-center px-6 py-12 ">
+            <img
+              class="w-36 h-36 mb-3 rounded-full shadow-lg"
+              src={kanafeh}
+              alt="Bonnie image"
+            />
+            <h5 class="mb-1 text-xl font-medium text-white">
+              {selectedOption}
+            </h5>
+          </div>
+        </div>
+        <div class="self-center w-full max-w-sm hadow container-one">
+          <div class="flex flex-col items-center pt-4 pb-10">
+            <img
+              class="w-36 h-36 mb-3 rounded-full shadow-lg"
+              src={kanafeh}
+              alt="Bonnie image"
+            />
+            <h5 class="mb-1 text-xl font-medium text-white">
+              {arr[after].name}
+            </h5>
+          </div>
         </div>
       </div>
     </section>
   );
-}
+};
+
+export default Menu;
